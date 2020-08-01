e2ca210a550c9deeef9fa0c4a19cd43f88c679ce
tung <tung.nguyen271193@gmail.com>
2020-08-01 14:39:39 +0300

    add collection item

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import SHOP_DATA from './shop.data';
 
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
@@ -12,7 +14,16 @@ class ShopPage extends React.Component {
   }
 
   render() {
-    return <div>SHOP PAGE</div>
+    const { collections } = this.state;
+    return (
+      <div className='shop-page'>
+        {
+          collections.map(({ id, ...otherSectionProps }) => (
+            <CollectionPreview key={id} {...otherSectionProps} />
+          ))
+        }
+      </div>
+    );
   }
 }
 
