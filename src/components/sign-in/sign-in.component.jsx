eb7b53e390f11cece12b3830c3016c83706cec72
tung <tung.nguyen271193@gmail.com>
2020-08-18 10:49:18 +0300

    complete sign  in funcion

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -18,27 +18,37 @@ class SingIn extends React.Component {
     this.setState({ email:'', password: '' })
   }
 
-  handleChange = event => {}
+  handleChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  }
 
   render() {
     return (
-      <div className='sign-in'>
+      <div className="sign-in">
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
-          <input name='email' type='email' value={this.state.email} required />
-          <lable>Email</lable>
           <input 
-            name='password' 
-            type='password' 
-            value={this.state.password}
-            onChange 
+            name="email" 
+            type="email" 
+            value={this.state.email}
+            onChange={this.handleChange} 
             required 
+          />
+          <lable>Email</lable>
+          <input
+            name="password"
+            type="password"
+            value={this.state.password}
+            onChange={this.handleChange}
+            required
           />
           <lable>Password</lable>
 
-          <input type='submit' value='Submit Form' />
+          <input type="submit" value="Submit Form" />
         </form>
       </div>
     );
